2718384fa994bc33162de31e3382533de0259809
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-10-22 13:11:12 +0300

    Test

--- src/main/java/com/example/olxpostman/OlxPostmanApplication.java
+++ src/main/java/com/example/olxpostman/OlxPostmanApplication.java
@@ -3,11 +3,29 @@ package com.example.olxpostman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
 @EnableFeignClients
 //@EnableWebSecurity
-public class OlxPostmanApplication {
+public class OlxPostmanApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+				.authorizeRequests(a -> a
+						.antMatchers("/", "/error", "/webjars/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.oauth2Login();
+		}
 
     public static void main(String[] args) {
         SpringApplication.run(OlxPostmanApplication.class, args);
