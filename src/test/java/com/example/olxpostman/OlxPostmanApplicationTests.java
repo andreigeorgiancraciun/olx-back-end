b4a7bf3ebb30dce46d1328a688437669f48da910
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-10-22 12:34:36 +0300

    Remove spring boot context test

--- src/test/java/com/example/olxpostman/OlxPostmanApplicationTests.java
+++ src/test/java/com/example/olxpostman/OlxPostmanApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.olxpostman;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OlxPostmanApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
