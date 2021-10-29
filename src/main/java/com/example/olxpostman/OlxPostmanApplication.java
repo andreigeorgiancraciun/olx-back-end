1a3491c61734a94c6f5ff433437df94a923a1d08
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-10-29 12:48:35 +0300

    remove security

--- src/main/java/com/example/olxpostman/OlxPostmanApplication.java
+++ src/main/java/com/example/olxpostman/OlxPostmanApplication.java
@@ -3,31 +3,34 @@ package com.example.olxpostman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableFeignClients
 @RestController
-public class OlxPostmanApplication {
+public class OlxPostmanApplication extends WebSecurityConfigurerAdapter {
 
-
-	/*@GetMapping("/user")
+	@GetMapping("/user")
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
 		return Collections.singletonMap("name", principal.getAttribute("name"));
 	}
-
-	@GetMapping("/error")
-	public String error(HttpServletRequest request) {
-		String message = (String) request.getSession().getAttribute("error.message");
-		request.getSession().removeAttribute("error.message");
-		return message;
-	}*/
 
     public static void main(String[] args) {
         SpringApplication.run(OlxPostmanApplication.class, args);
     }
 
-	/*@Override
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http
@@ -46,5 +49,5 @@ public class OlxPostmanApplication {
 				)
 				.oauth2Login();
 		// @formatter:on
-	}*/
+	}
 }
