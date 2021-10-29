823c71093be61314e00c27f4ce33afbf8761682b
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-10-29 12:06:35 +0300

    change toke url

--- src/main/java/com/example/olxpostman/OlxPostmanApplication.java
+++ src/main/java/com/example/olxpostman/OlxPostmanApplication.java
@@ -3,19 +3,7 @@ package com.example.olxpostman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableFeignClients
