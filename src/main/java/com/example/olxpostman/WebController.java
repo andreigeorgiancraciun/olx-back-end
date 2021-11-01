285f1b29f6a2f588fb55f8db0f8f07c38cfa4172
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-11-01 11:05:51 +0200

    replace redirect url

---
+++ src/main/java/com/example/olxpostman/WebController.java
@@ -0,0 +1,16 @@
+package com.example.olxpostman;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/auth/connect")
+	public String authConnect(Model model, Principal principal) {
+		return "auth";
+	}
+}
