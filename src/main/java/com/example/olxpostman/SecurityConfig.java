105833543c2f3cbb693c828568fa14bd9c9851f5
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-11-06 11:53:14 +0200

    danuts version 2

--- src/main/java/com/example/olxpostman/SecurityConfig.java
+++ src/main/java/com/example/olxpostman/SecurityConfig.java
@@ -3,13 +3,18 @@ package com.example.olxpostman;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests()
+		http.antMatcher("/**")
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				)
+				.authorizeRequests()
 				.antMatchers("/").permitAll()
 				.anyRequest().authenticated()
 				.and()
