93894ac8e091509679cf43bb2aad102191465a03
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-11-06 10:19:20 +0200

    danuts version

--- src/main/java/com/example/olxpostman/SecurityConfig.java
+++ src/main/java/com/example/olxpostman/SecurityConfig.java
@@ -7,32 +7,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	/*@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http
-				.authorizeRequests(a -> a
-						.antMatchers("/", "/error", "/webjars/**").permitAll()
-						.anyRequest().authenticated()
-				)
-				.exceptionHandling(e -> e
-						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-				)
-				.csrf(c -> c
-						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-				)
-				.logout(l -> l
-						.logoutSuccessUrl("/").permitAll()
-				)
-				.oauth2Login();
-		// @formatter:on
-	}*/
-
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/", "/actuator").permitAll()
-				.antMatchers("/classified").authenticated()
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests()
+				.antMatchers("/").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.oauth2Login();
