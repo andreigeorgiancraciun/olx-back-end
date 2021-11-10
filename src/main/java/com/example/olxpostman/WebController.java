28db58a606a7450637d190f0836a597c67d404d2
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-11-10 09:57:16 +0200

    vladut version 1

--- src/main/java/com/example/olxpostman/WebController.java
+++ src/main/java/com/example/olxpostman/WebController.java
@@ -5,14 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class WebController {
 
-	@GetMapping("/classified")
+	/*@GetMapping("/classified")
 	public String classified(@AuthenticationPrincipal OAuth2User principal, Model model) {
 		model.addAttribute("name", principal.getAttribute("name"));
 		model.addAttribute("authorities", principal.getAuthorities());
 		return "classified";
+	}*/
+
+	@GetMapping("/user")
+	@ResponseBody
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@GetMapping("/error")
+	@ResponseBody
+	public String error(HttpServletRequest request) {
+		String message = (String) request.getSession().getAttribute("error.message");
+		request.getSession().removeAttribute("error.message");
+		return message;
 	}
 }
