940c88ee0ec504a3e55a8f74e36996b904e1150b
Craciun Andrei <andrei.craciun@ibapplications.com>
2021-11-10 16:23:44 +0200

    change from yaml to properties

--- src/main/java/com/example/olxpostman/SecurityConfiguration.java
+++ src/main/java/com/example/olxpostman/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
